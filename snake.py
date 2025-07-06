47ef6653c310e81522d9e9d1369bd1b51e423af9
lucaschm <luca.schmitt@haw-hamburg.de>
2025-07-06 15:57:34 +0200

    finish snake framework

--- snake.py
+++ snake.py
@@ -1,38 +1,52 @@
+import random
+
 import numpy as np
-from enum import Enum, auto
 
 class Movement():
     UNCHANGED = np.array([0, 0])
-    UP = np.array([0, 1])
+    UP = np.array([0, -1])
     LEFT = np.array([-1, 0])
-    DOWN = np.array([0, -1])
+    DOWN = np.array([0, 1])
     RIGHT = np.array([1, 0])
 
-class Health(Enum):
-    ALIVE = "alive"
-    DEAD = "dead"
+class Map():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_center_coordinates(self):
+        return [round(self.width/2.0), round(self.height/2.0)]
+
+    def is_in_map(self, coordinates) -> bool:
+        x = coordinates[0]
+        y = coordinates[1]
+        return -1 < x < self.width and -1 < y < self.height
+
+    def is_boundry(self, coordinates) -> bool:
+        return not self.is_in_map(coordinates)
+
+    def get_random_coordinates(self) -> list:
+        x = random.randint(0, self.width - 1)
+        y = random.randint(0, self.height - 1)
+        return [x, y]
 
 class Snake():
     def __init__(self, x, y) -> None:
         self.body = [np.array([x, y])]
         self.direction = Movement.UNCHANGED
 
-
-
     def opposite_direction(self, move:Movement) -> bool:
-        return sum(move + self.direction) == 0
+        return (move + self.direction == Movement.UNCHANGED).all()
 
     def update_direction(self, move:Movement):
-        if move != Movement.UNCHANGED and not self.opposite_direction(move):
+        if (move != Movement.UNCHANGED).any() and not self.opposite_direction(move):
             self.direction = move
-
-
-
-    def get_next_head(self):
-        return self.body.head() + self.direction
 
     def head(self):
         return self.body[-1]
+
+    def get_next_head(self):
+        return self.head() + self.direction
 
     def move(self, growing=False):
         new_head = self.get_next_head()
@@ -41,28 +55,40 @@ class Snake():
         if not growing:
             self.body.pop(0) # remove last tail element
 
-    def is_snake_body(self, coordinates):
+    def is_snake_body(self, coordinates, ignore_tail_elemts:int=1):
         result = False
-        for i in range(1, self.body.len()): # ignore last tail element
+        for i in range(ignore_tail_elemts, len(self.body)):
             if (self.body[i] == coordinates).all():
                 result = True
         return result
 
 class Game():
-    ... # TODO
+    def __init__(self, width, height) -> None:
+        self.map = Map(width, height)
+        center = self.map.get_center_coordinates()
+        self.snake = Snake(x=center[0], y=center[1])
+        self.food = None
+        self.generate_food()
+        self.game_over = False
+        self.score = 0
 
-class Map():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def generate_food(self):
+        while True:
+            food_candidate = self.map.get_random_coordinates()
+            if not self.snake.is_snake_body(food_candidate, ignore_tail_elemts=0):
+                self.food = np.array(food_candidate)
+                break
 
-    def get_center_position(self):
-        return [round(self.width/2.0), round(self.height/2.0)]
+    def evaluate(self, move:Movement):
+        self.snake.update_direction(move)
+        next_head = self.snake.get_next_head()
 
-
-if __name__ == '__main__':
-    pos = np.array([5, 6])
-    segment = np.array([4, 6])
-
-    print((pos == segment).all())
-
+        if (self.map.is_boundry(next_head) or self.snake.is_snake_body(next_head)):
+            print("Game Over!")
+            self.game_over = True
+        elif (next_head == self.food).all():
+            self.snake.move(growing=True)
+            self.generate_food()
+            self.score += 1
+        else:
+            self.snake.move(growing=False)
